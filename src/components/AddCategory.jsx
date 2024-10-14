1f898a49400af600c4e89739129dad3cde89f903
Victor E González P <vemiliogp@gmail.com>
2024-10-15 00:12:03 +0200

    Initial commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputValue.trim().length === 0) return;
+
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Search gif"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+};
